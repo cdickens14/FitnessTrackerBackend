455d03bab66823a263f4af3138c369c07e768006
Christine Dickens <christine.dickens@utexas.edu>
2023-03-29 16:05:22 -0500

    activities and routines router

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const { getAllActivities } = require('../db/activities.js');
-// const { getUserByUsername, createUser } = require('../db/users.js');
+const { getAllActivities, getActivityById, createActivity } = require('../db/activities.js');
+const { createUser } = require('../db/users.js');
+const { getAllRoutines, createRoutine, getRoutineById } = require('../db/routines.js');
 
 // GET /api/health
 router.get('/health', async (req, res, next) => {
@@ -12,6 +13,17 @@ router.get('/health', async (req, res, next) => {
 // ROUTER: /api/users
 const usersRouter = require('./users');
 router.use('/users', usersRouter);
+
+router.post('/register', async(req, res, next) => {
+    try {
+        const user = await createUser();
+        res.send({user});
+    } catch (err) {
+        console.log(err);
+    }
+    next();
+    
+});
 // router.post('/users/register', async (req, res, next) => {
 //     const { username, password } = req.body;
 
@@ -49,9 +61,60 @@ router.get('/activities', async (req, res, next) => {
     }
 });
 
+router.get('/:activityId/routines', async (req, res, next) => {
+    try {
+        const activity = await getActivityById(req.params.activityId);
+        res.send(activity);
+    } catch (err) {
+     throw Error("That activity does not exist");
+    }
+//does not throw err message in browser
+   next();
+});
+
+router.post('/activities', async (req, res, next) => {
+    try {
+        const activity = await createActivity(req.body);
+        res.send(activity);
+    } catch (err) {
+      next (err);
+    }
+});
+
+
 // ROUTER: /api/routines
 const routinesRouter = require('./routines');
 router.use('/routines', routinesRouter);
+router.get('/', async (req, res, next) => {
+    try {
+        const routines = await getAllRoutines();
+    res.send(routines);
+    } catch (err) {
+      console.log(err);
+    }
+   next(); 
+});
+
+router.post('/routines', async (req, res, next) => {
+    try {
+        const routines = await createRoutine(req.body);
+        res.send(routines);
+    } catch (err) {
+      console.log(err);
+    }
+    next();
+});
+
+router.post('/:routineId/activities', async (req, res, next) => {
+    try {
+        const routineId = await getRoutineById(req.params.routineId);
+    res.send(routineId);
+    } catch (err) {
+      console.log(err);
+    }
+    next();
+});
+
 
 // ROUTER: /api/routine_activities
 const routineActivitiesRouter = require('./routineActivities');
--- api/routines.js
+++ api/routines.js
@@ -1,14 +1,49 @@
 const express = require('express');
 const router = express.Router();
+const { getAllRoutines, createRoutine, getRoutineById, destroyRoutine }= require('../db/routines.js');
 
 // GET /api/routines
-
+router.get('/', async (req, res, next) => {
+    try {
+        const routines = await getAllRoutines();
+    res.send(routines);
+    } catch (err) {
+      console.log(err);
+    }
+    next();
+});
 // POST /api/routines
+router.post('/routines', async (req, res, next) => {
+    try {
+        const routines = await createRoutine(req.body);
+        res.send(routines);
+    } catch (err) {
+      console.log(err);
+    }
+    next();
+});
 
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
-
+router.delete('/:routineId', async (req, res, next) => {
+    try {
+        const routineId = await destroyRoutine(req.params.routineId);
+        res.send(routineId);
+    } catch (err) {
+      console.log(err);
+    }
+    next();
+})
 // POST /api/routines/:routineId/activities
+router.post('/:routineId/activities', async (req, res, next) => {
+    try {
+        const routineId = await getRoutineById(req.params.routineId);
+    res.send(routineId);
+    } catch (err) {
+      console.log(err);
+    }
+    next();
+});
 
 module.exports = router;
