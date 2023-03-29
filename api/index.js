db5c8bbc8fbb01f3b9c7a64d55c13502889cb488
Soren Axelson <sorenaxelson@gmail.com>
2023-03-29 15:14:23 -0500

    iwryfuh

--- api/index.js
+++ api/index.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 // GET /api/health
-router.get('/health', async (req, res, next) => {
-});
+router.get("/health", async (req, res, next) => {});
 
 // ROUTER: /api/users
-const usersRouter = require('./users');
-router.use('/users', usersRouter);
-
+const usersRouter = require("./users");
+router.use("/users", usersRouter);
+router.post("/users/register", async (req, res, next) => {
+  const user = await createUser();
+  res.send(user);
+});
 // ROUTER: /api/activities
-const activitiesRouter = require('./activities');
-router.use('/activities', activitiesRouter);
+const activitiesRouter = require("./activities");
+router.use("/activities", activitiesRouter);
 
 // ROUTER: /api/routines
-const routinesRouter = require('./routines');
-router.use('/routines', routinesRouter);
+const routinesRouter = require("./routines");
+router.use("/routines", routinesRouter);
 
 // ROUTER: /api/routine_activities
-const routineActivitiesRouter = require('./routineActivities');
-router.use('/routine_activities', routineActivitiesRouter);
+const routineActivitiesRouter = require("./routineActivities");
+router.use("/routine_activities", routineActivitiesRouter);
 
 module.exports = router;
--- api/users.js
+++ api/users.js
@@ -1,9 +1,14 @@
 /* eslint-disable no-useless-catch */
+const { createUser } = require("./db/users.js");
+
 const express = require("express");
 const router = express.Router();
 
 // POST /api/users/register
-
+router.post("/users/register", async (req, res, next) => {
+  const user = await createUser();
+  res.send(user);
+});
 // POST /api/users/login
 
 // GET /api/users/me
