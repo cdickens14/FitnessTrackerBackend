ac77e4c838d7ff3a3547beed9e5b815164c63f7a
Soren Axelson <sorenaxelson@gmail.com>
2023-04-05 14:12:09 -0500

    4/5/23

--- api/users.js
+++ api/users.js
@@ -2,81 +2,94 @@
 const express = require("express");
 const usersRouter = express.Router();
 const jwt = require("jsonwebtoken");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
 const { getPublicRoutinesByUser } = require("../db/routines.js");
-const { getUserByUsername, createUser } = require("../db/users.js");
+const { getUserByUsername, createUser, getUser } = require("../db/users.js");
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
   next();
 });
 // POST /api/users/register
-usersRouter.post ('/register', async (req, res, next) => {
+usersRouter.post("/register", async (req, res, next) => {
   const { username, password } = req.body;
-
+  // console.log("BODY", req.body);
   try {
+    // console.log("USERNAME", username);
     const _user = await getUserByUsername(username);
-    if(_user) {
+    if (_user) {
       next({
-        name: 'UserExistsError',
-        message: 'A user by that username already exists'
+        name: "UserExistsError",
+        message: `User ${username} is already taken.`,
+        error: "UserExistsError",
       });
     }
-
     const user = await createUser(req.body);
-    const token = jwt.sign({
-      id: user.id,
-      username
-    })
+    // if (user.password.length < 8) {
+    //   next({
+    //     message: "Password Too Short!",
+    //     error: "Password Too Short!",
+    //     name: "Password Too Short!",
+    //   });
+    // }
+    // console.log("_USER", _user);
+    // console.log("USER", user);
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      "secret"
+    );
     res.send({
-      message: 'Thank you for signing up',
-      token
+      user,
+      message: "Thank you for signing up",
+      token,
     });
   } catch (err) {
-    next (err);
+    next(err);
   }
 });
-  
 // POST /api/users/login
-usersRouter.post('/login', async (req, res, next) => {
+usersRouter.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
 
   if (!username || !password) {
     next({
-      name: "MissingCredentialsError",
-      message: "Please supply both username and password"
+      name: "IncorrectCredentialsError",
+      message: "Please supply both username and password",
+      error: "IncorrectCredentialsError",
     });
   }
 
   try {
-    const user = await getUserByUsername(username);
-
-    if (user && user.password === password) {
-      res.send({ message: "You're logged in!"});
+    const user = await getUser({ username, password });
+    console.log("USER.PASSWORD", user.password);
+    console.log("PASSWORD", password);
+    if (user.password === password) {
+      res.send({ message: "You're logged in!" });
     } else {
       next({
         name: "IncorrectCredentialsError",
-        message: "Username or password is incorrect"
+        message: "Username or password is incorrect",
       });
     }
   } catch (err) {
-    console.log (err);
-    next (err);
+    console.log(err);
+    next(err);
   }
-})
+});
 // GET /api/users/me
 
 // GET /api/users/:username/routines
-usersRouter.get(':username/routines', async (req, res, next) => {
+usersRouter.get(":username/routines", async (req, res, next) => {
   try {
     const { username } = req.params;
     const publicRoutinesByUser = await getPublicRoutinesByUser(username);
     res.send(publicRoutinesByUser);
   } catch (err) {
-    next (err);
+    next(err);
   }
-
 });
-
 
 module.exports = usersRouter;
