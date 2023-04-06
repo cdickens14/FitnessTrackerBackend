f1986d44718bd06e8acfb5ecb2825430fe93bfe5
Soren Axelson <sorenaxelson@gmail.com>
2023-04-06 11:24:13 -0500

    4/6/23

--- src/index.js
+++ src/index.js
@@ -1,31 +1,28 @@
-import React, { useEffect, useState } from 'react';
-import { createRoot } from 'react-dom/client';
-import axios from 'axios';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Activities from './Activities.js';
-import Header from './Header.js';
-import Routines from './Routines.js';
+import React, { useEffect, useState } from "react";
+import { createRoot } from "react-dom/client";
+import axios from "axios";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Activities from "./Activities.js";
+import Header from "./Header.js";
+import Routines from "./Routines.js";
 
 const App = () => {
-    
-    return (
-        <React.Fragment>
-            <Header />
-            {/* <Routes>
-                <Route path='/api/activities' element={<Activities />}></Route>
-                <Route path='/api/routines' element={<Routines />}></Route>
-            </Routes> */}
-            <Activities />
-            <Routines />
-           
-        </React.Fragment>
-        
-    )
-}
+  return (
+    <React.Fragment>
+      <Header />
+      <Routes>
+        <Route path="/api/activities" element={<Activities />}></Route>
+        <Route path="/api/routines" element={<Routines />}></Route>
+      </Routes>
+      <Activities />
+      <Routines />
+    </React.Fragment>
+  );
+};
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 root.render(
-<HashRouter>
+  <HashRouter>
     <App />
-</HashRouter>
+  </HashRouter>
 );
