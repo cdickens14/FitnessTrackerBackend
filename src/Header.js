273085f315306472934cea641b6c70623cac7244
Soren Axelson <sorenaxelson@gmail.com>
2023-04-18 00:26:53 -0500

    header html and css

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
 
 const Header = () => {
-    return (
-        <h1>Fitness Tracker</h1>
-    )
-}
+  return (
+    <div id="header">
+      <nav>
+        <ul>
+          <li>
+            <a href="#"> Home </a>
+          </li>
+          <li>
+            <a href="#"> Public Routines </a>
+          </li>
+          <li>
+            <a href="#"> Public Activities </a>
+          </li>
+          <li>
+            <a href="#"> Your Routines </a>
+          </li>
+          <li>
+            <a href="#"> Login </a>
+          </li>
+          <li>
+            <a href="#"> Sign Up </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Header;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,61 @@
-import React, { useState, useEffect } from 'react';
-import { createRoot } from 'react-dom/client';
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import Activities from './Activities.js';
-import Home from './Home.js';
-import Routines from './Routines.js';
-import Register from './Register.js';
-import Login from './Login.js';
-import Header from './Header.js';
-import MyRoutines from './MyRoutines.js';
+import React, { useState, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Activities from "./Activities.js";
+import Home from "./Home.js";
+import Routines from "./Routines.js";
+import Register from "./Register.js";
+import Login from "./Login.js";
+import Header from "./Header.js";
+import MyRoutines from "./MyRoutines.js";
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const logOut = (token) => {
     setIsLoggedIn(false);
-    window.localStorage.removeItem('token', `${token}`);
-  }
+    window.localStorage.removeItem("token", `${token}`);
+  };
 
-    return (
-        <React.Fragment>
-          <Header />
+  return (
+    <React.Fragment>
+      {/* <Header />
 
-            <Routes>
-                <Route path='/api' element={<Home isLoggedIn={isLoggedIn}/>}></Route>
-                <Route path='/api/users/register' element={<Register setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn}/>}></Route>
-                <Route path='/api/users/login' element={<Login isLoggedin={isLoggedIn } setIsLoggedIn={setIsLoggedIn}/>}></Route>
-                <Route path='/api/activities' element={<Activities isLoggedIn={isLoggedIn}/>}></Route>
-                <Route path='/api/routines' element={<Routines isLoggedIn={isLoggedIn}/>}></Route>
-                <Route path='/api/users/me' element={<MyRoutines />}></Route>
-            </Routes>
+      <Routes>
+        <Route path="/api" element={<Home isLoggedIn={isLoggedIn} />}></Route>
+        <Route
+          path="/api/users/register"
+          element={
+            <Register setIsLoggedIn={setIsLoggedIn} isLoggedIn={isLoggedIn} />
+          }
+        ></Route>
+        <Route
+          path="/api/users/login"
+          element={
+            <Login isLoggedin={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+          }
+        ></Route>
+        <Route
+          path="/api/activities"
+          element={<Activities isLoggedIn={isLoggedIn} />}
+        ></Route>
+        <Route
+          path="/api/routines"
+          element={<Routines isLoggedIn={isLoggedIn} />}
+        ></Route>
+        <Route path="/api/users/me" element={<MyRoutines />}></Route>
+      </Routes>
 
-            {
-                isLoggedIn === true ?
-                <button onClick={() => logOut()}>Logout</button> : null
-            }
-          
-        </React.Fragment>
-        
-    )
-}
+      {isLoggedIn === true ? (
+        <button onClick={() => logOut()}>Logout</button>
+      ) : null} */}
+    </React.Fragment>
+  );
+};
 
-const root = createRoot(document.getElementById('root'));
-root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
-);
+// const root = createRoot(document.getElementById("root"));
+// root.render(
+//   <HashRouter>
+//     <App />
+//   </HashRouter>
+// );
